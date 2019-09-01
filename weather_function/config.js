9e567a48a6e94b836ee9e8000de86e8d531c3aff
Dmytro <krasilnikov.dmytro@gmail.com>
2019-09-01 18:34:46 +0300

    Url transfer to a separate module

--- weather_function/config.js
+++ weather_function/config.js
@@ -1,8 +1,14 @@
+const apiKey = 'b078a7d81849e7d2b796924e54583a40';
+const yourCity = 'dnipro';
+
 module.exports.apiKey = () => {
-  let apiKey = 'b078a7d81849e7d2b796924e54583a40';
   return apiKey;
 }
 module.exports.yourCity = () => {
-  let yourCity = 'dnipro';
   return yourCity;
+}
+
+module.exports.yourUrl = () => {
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${yourCity}&appid=${apiKey}`;
+  return url;
 }
--- weather_function/index.js
+++ weather_function/index.js
@@ -2,9 +2,8 @@ const config = require('./config.js');
 const request = require('request');
 config.apiKey();
 config.yourCity();
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${config.yourCity()}&appid=${config.apiKey()}`;
 
-request(url, (err, response, body) => {
+request(config.yourUrl(), (err, response, body) => {
   if(err){
     console.log('Upss, something wrong!:', err);
   } else {
