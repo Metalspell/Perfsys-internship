b267173593d137c8c39a422eaa5d8ffbcb97faca
Dmytro <krasilnikov.dmytro@gmail.com>
2019-10-20 19:58:25 +0300

    Refactoring existing code and fix some mistakes

--- aws serverless project/Dmitry-first-attempt/handler.js
+++ aws serverless project/Dmitry-first-attempt/handler.js
@@ -1,23 +1,19 @@
-'use strict'
-
 const fetch = require("node-fetch");
-let apiKey = 'b078a7d81849e7d2b796924e54583a40';
-let yourCity = 'dnipro';
-let url = `http://api.openweathermap.org/data/2.5/weather`;
-
+// const axios = require('axios');
 const AWS = require('aws-sdk');
-AWS.config.update({ region: "eu-central-1"});
+AWS.config.update({ region: process.env.REGION });
 let params = null;
+const documentClient = new AWS.DynamoDB.DocumentClient({region: process.env.REGION});
 
 module.exports.putWeatherData = async (event, context) => {
-  const weather = await fetch(`${url}?q=${yourCity}&appid=${apiKey}`);
-  const documentClient = new AWS.DynamoDB.DocumentClient({ region: "eu-central-1"});
+  const weather = await fetch(`${process.env.URL}?q=${process.env.YOUR_CITY}&appid=${process.env.API_KEY}`);
+  // const weather = await axios.get(`${process.env.URL}?q=${process.env.YOUR_CITY}&appid=${process.env.API_KEY}`);
 
   let responseBody = "";
   let statusCode = 0;
 
   let params = {
-    TableName: "first-attempt-dev",
+    TableName: process.env.TABLE_NAME,
     Item: {
       id: "1",
       weatherdata: await weather.json()
@@ -27,16 +23,16 @@ module.exports.putWeatherData = async (event, context) => {
   try {
     data = await documentClient.put(params).promise();
     responseBody = JSON.stringify(data);
-    statusCode = 201;
+    statusCode = 200;
   } catch (err) {
     responseBody = `Unable to put data: ${err}`;
-    statusCode  = 403;
+    statusCode  = 500;
   }
 
   const response = {
     statusCode: statusCode,
     headers: {
-      "Content-Type" : "application/json"
+      "Content-Type" : "application/string"
     },
     body: responseBody
   };
@@ -44,13 +40,12 @@ module.exports.putWeatherData = async (event, context) => {
 };
 
 module.exports.getWeatherData = async (event, context) => {
-  const documentClient = new AWS.DynamoDB.DocumentClient({ region: "eu-central-1"});
 
   let responseBody = "";
   let statusCode = 0;
 
   let params = {
-    TableName: "first-attempt-dev",
+    TableName: process.env.TABLE_NAME,
     Key: {
       id:'1'
     }
@@ -59,16 +54,16 @@ module.exports.getWeatherData = async (event, context) => {
   try {
     let data = await documentClient.get(params).promise();
     responseBody = JSON.stringify(data);
-    statusCode = 201;
+    statusCode = 200;
   } catch (err) {
     responseBody = `Unable to read data: ${err}`;
-    statusCode  = 403;
+    statusCode  = 500;
   }
 
   const response = {
     statusCode: statusCode,
     headers: {
-      "Content-Type" : "application/json"
+      "Content-Type" : "application/string"
     },
     body: responseBody
   };
